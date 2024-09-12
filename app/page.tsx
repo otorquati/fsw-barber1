cc73e4069b34103f17d2f9929d6b1b1035d26f24
otorquati <torquati.osvaldo@gmail.com>
2024-09-12 09:14:22 -0300

    fix: Header problems

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,34 @@
+import { SearchIcon } from "lucide-react";
+import Header from "./_components/header";
 import { Button } from "./_components/ui/button";
+import { Input } from "./_components/ui/input";
+import Image from "next/image";
 
 const Home = () => {
-  return ( 
-        <Button>Teste</Button>
-   );
-}
- 
+  return (
+    <div>
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Ola, Osvaldo</h2>
+        <p>Quinta-feira, 12 de setembro</p>
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Faça sua busca..." />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            alt="Agende com os melhores FSW Barber"
+            src="/banner-01.png"
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default Home;
