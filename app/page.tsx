7cafb15bbd2c9e819586fbd9e9fc1e2d8287a6cb
otorquati <torquati.osvaldo@gmail.com>
2024-09-15 19:44:48 -0300

    feat: add BarberShopItem component

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,12 @@ import Image from "next/image";
 import { Card, CardContent } from "./_components/ui/card";
 import { Badge } from "./_components/ui/badge";
 import { Avatar, AvatarImage } from "./_components/ui/avatar";
+import { db } from "./_lib/prisma";
+import BarbershopItem from "./_components/barbershop-item";
 
-const Home = () => {
+const Home = async () => {
+  // chamar banco de dados
+  const barbershops = await db.barbershop.findMany({});
   return (
     <div>
       <Header />
@@ -34,8 +38,11 @@ const Home = () => {
         </div>
 
         {/* AGENDAMENTO */}
-        <Card className="mt-6">
-          <CardContent className="flex justify-between p-0">
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Agendamentos
+        </h2>
+        <Card>
+          <CardContent className="mb-3 mt-6 flex justify-between p-0">
             {/* ESQUERDA */}
             <div className="flex flex-col gap-2 py-5 pl-5">
               <Badge className="w-fit">Confirmado</Badge>
@@ -47,6 +54,7 @@ const Home = () => {
                 <p className="text-sm">Barbearia TorQuality</p>
               </div>
             </div>
+
             {/* DIREITA */}
             <div className="flex flex-col items-center justify-center border-l-2 border-solid p-5">
               <p className="text-sm">Agosto</p>
@@ -55,6 +63,16 @@ const Home = () => {
             </div>
           </CardContent>
         </Card>
+
+        {/* RECOMENDADOS */}
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Recomendados
+        </h2>
+        <div className="flex gap-4 overflow-auto [&::-webkit-scrollbar]:hidden">
+          {barbershops.map((barbershop) => (
+            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
+        </div>
       </div>
     </div>
   );
