22b266e1951a4b3b35bb9269e1a9c7c6e27c5c1b
otorquati <torquati.osvaldo@gmail.com>
2024-09-23 07:52:12 -0300

    feat: add side bar

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,7 +1,18 @@
 import Image from "next/image";
 import { Card, CardContent } from "./ui/card";
 import { Button } from "./ui/button";
-import { MenuIcon } from "lucide-react";
+import { CalendarIcon, HomeIcon, LogOutIcon, MenuIcon } from "lucide-react";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
+import { quickSearchOptions } from "../_constants/quickSearchItems";
+import { Avatar, AvatarImage } from "./ui/avatar";
+import Link from "next/link";
 
 const Header = () => {
   return (
@@ -9,9 +20,68 @@ const Header = () => {
       <Card>
         <CardContent className="flex flex-row items-center justify-between p-5">
           <Image alt="FSW Barber" src="/logo.png" height={18} width="120" />
-          <Button size="icon" variant="outline">
-            <MenuIcon />
-          </Button>
+          <Sheet>
+            <SheetTrigger>
+              <Button size="icon" variant="outline">
+                <MenuIcon />
+              </Button>
+            </SheetTrigger>
+            <SheetContent>
+              <SheetHeader>
+                <SheetTitle>Menu</SheetTitle>
+              </SheetHeader>
+
+              <div className="flex items-center gap-3 border-b border-solid py-5">
+                <Avatar>
+                  <AvatarImage src="/Torquati_Avatar.jpg" />
+                </Avatar>
+                <div>
+                  <p className="font-bold">Osvaldo Torquati</p>
+                  <p className="text-sm">torquati.osvaldo@outlook.com</p>
+                </div>
+              </div>
+
+              <div className="flex flex-col gap-2 border-b border-solid py-5">
+                <Button className="justify-start gap-2" variant="ghost" asChild>
+                  <SheetClose asChild>
+                    <Link href="/">
+                      <HomeIcon size={18} />
+                      Início
+                    </Link>
+                  </SheetClose>
+                </Button>
+                <Button className="justify-start gap-2" variant="ghost">
+                  <CalendarIcon size={18} />
+                  Agendamentos
+                </Button>
+              </div>
+
+              <div className="flex flex-col gap-2 border-b border-solid py-5">
+                {quickSearchOptions.map((option) => (
+                  <Button
+                    key={option.title}
+                    className="justify-start gap-2"
+                    variant="ghost"
+                  >
+                    <Image
+                      alt={option.title}
+                      src={option.imageurl}
+                      height={18}
+                      width={18}
+                    />
+                    {option.title}
+                  </Button>
+                ))}
+              </div>
+
+              <div className="flex flex-col gap-2 py-5">
+                <Button className="justify-start gap-2" variant="ghost">
+                  <LogOutIcon size={18} />
+                  Sair da Conta
+                </Button>
+              </div>
+            </SheetContent>
+          </Sheet>
         </CardContent>
       </Card>
     </div>
