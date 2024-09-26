c5152a5ef2e8d4382ad6899beebb4f3989b875ca
otorquati <torquati.osvaldo@gmail.com>
2024-09-26 09:09:22 -0300

    chore: add next auth setup

--- app/_components/sidebar-sheet.tsx
+++ app/_components/sidebar-sheet.tsx
@@ -1,11 +1,18 @@
 import Image from "next/image";
 import { Button } from "./ui/button";
-import { CalendarIcon, HomeIcon, LogOutIcon } from "lucide-react";
+import { CalendarIcon, HomeIcon, LogInIcon, LogOutIcon } from "lucide-react";
 
 import { quickSearchOptions } from "../_constants/quickSearchItems";
-import { Avatar, AvatarImage } from "./ui/avatar";
 import Link from "next/link";
 import { SheetClose, SheetContent, SheetHeader, SheetTitle } from "./ui/sheet";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog";
 
 const SidebarSheet = () => {
   return (
@@ -14,14 +21,40 @@ const SidebarSheet = () => {
         <SheetTitle>Menu</SheetTitle>
       </SheetHeader>
 
-      <div className="flex items-center gap-3 border-b border-solid py-5">
-        <Avatar>
+      <div className="flex items-center justify-between gap-3 border-b border-solid py-5">
+        <h2 className="font-bold">Olá, faça seu login</h2>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button size="icon">
+              <LogInIcon size={18} />
+            </Button>
+          </DialogTrigger>
+          <DialogContent className="w-[90%]">
+            <DialogHeader>
+              <DialogTitle>Faça login na plataforma</DialogTitle>
+              <DialogDescription>
+                Conecte-se usando sua conta Google
+              </DialogDescription>
+            </DialogHeader>
+            <Button variant="outline" className="gap-2">
+              <Image
+                alt="Fazer login com o Google"
+                src="/google.svg"
+                width={18}
+                height={18}
+              />
+              Google
+            </Button>
+          </DialogContent>
+        </Dialog>
+
+        {/*  <Avatar>
           <AvatarImage src="/Torquati_Avatar.jpg" />
         </Avatar>
         <div>
           <p className="font-bold">Osvaldo Torquati</p>
           <p className="text-sm">torquati.osvaldo@outlook.com</p>
-        </div>
+        </div> */}
       </div>
 
       <div className="flex flex-col gap-2 border-b border-solid py-5">
