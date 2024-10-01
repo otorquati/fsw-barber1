53725e89797ae758a2fb4176feee600b98abd271
otorquati <torquati.osvaldo@gmail.com>
2024-10-01 11:45:47 -0300

    Feat: starting logo return home

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -4,13 +4,17 @@ import { Sheet, SheetTrigger } from "./ui/sheet";
 import { Button } from "./ui/button";
 import { MenuIcon } from "lucide-react";
 import SidebarSheet from "./sidebar-sheet";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <div>
       <Card>
         <CardContent className="flex flex-row items-center justify-between p-5">
-          <Image alt="FSW Barber" src="/logo.png" height={18} width="120" />
+          <Link href="/">
+            <Image alt="FSW Barber" src="/logo.png" height={18} width="120" />
+          </Link>
+
           <Sheet>
             <SheetTrigger>
               <Button size="icon" variant="outline">
