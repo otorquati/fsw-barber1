d546a1cba0e66903df652b5df7176e839666df01
otorquati <torquati.osvaldo@gmail.com>
2024-12-04 17:23:59 -0300

    feat: ensure bookings are listed on bookings page

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,27 +1,6 @@
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import NextAuth from "next-auth";
-import { Adapter } from "next-auth/adapters";
-import GoogleProvider from "next-auth/providers/google";
-import { db } from "@/app/_lib/prisma";
+import { authOptions } from "@/app/_lib/auth";
 
-const handler = NextAuth({
-  adapter: PrismaAdapter(db) as Adapter,
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
-  ],
-  callbacks: {
-    async session({ session, user }) {
-      session.user = {
-        ...session.user,
-        id: user.id,
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      } as any;
-      return session;
-    },
-  },
-});
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
