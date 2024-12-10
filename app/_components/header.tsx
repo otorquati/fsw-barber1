7d20bf97a89f3a8046bb096f5b5b93e7e9b08300
otorquati <torquati.osvaldo@gmail.com>
2024-12-10 16:37:26 -0300

    refact: change components header and footer to use semantic tags

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -8,22 +8,24 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <Card>
-      <CardContent className="flex flex-row items-center justify-between p-5">
-        <Link href="/">
-          <Image alt="FSW Barber" src="/logo.png" height={18} width={120} />
-        </Link>
+    <header>
+      <Card>
+        <CardContent className="flex flex-row items-center justify-between p-5">
+          <Link href="/">
+            <Image alt="FSW Barber" src="/logo.png" height={18} width={120} />
+          </Link>
 
-        <Sheet>
-          <SheetTrigger asChild>
-            <Button size="icon" variant="outline" asChild>
-              <MenuIcon />
-            </Button>
-          </SheetTrigger>
-          <SidebarSheet />
-        </Sheet>
-      </CardContent>
-    </Card>
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button size="icon" variant="outline" asChild>
+                <MenuIcon />
+              </Button>
+            </SheetTrigger>
+            <SidebarSheet />
+          </Sheet>
+        </CardContent>
+      </Card>
+    </header>
   );
 };
 
